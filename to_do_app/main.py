c6e9d5e5f3b9ce6573355e0b48fb953f8b4ff4bf
Eva Keller <eva.keller1@gmail.com>
2026-08-23 20:35:34 +0100

    add edit feature

--- to_do_app/main.py
+++ to_do_app/main.py
@@ -1,6 +1,6 @@
 todos = []
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
     user_action = user_action.strip()
     match user_action:
         case "add":
@@ -9,6 +9,11 @@ while True:
         case "show":
             for item in todos:
                 print(item)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case "exit":
             break
 print("Bye!")
